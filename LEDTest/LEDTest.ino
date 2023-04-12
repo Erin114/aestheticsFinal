a799c5fa54f38ed051f8d5acea37399663e69468
Erin <asf5947@g.rit.edu>
2023-04-12 13:26:43 -0400

    added coordinate instructions

--- LEDTest/LEDTest.ino
+++ LEDTest/LEDTest.ino
@@ -13,7 +13,7 @@
 LedControl lc=LedControl(8,6,7,8);
 
 /* we always wait a bit between updates of the display */
-unsigned long delaytime=500;
+unsigned long delaytime=100;
 
 /* 
  This time we have more than one device. 
@@ -38,19 +38,25 @@ void loop() {
   //read the number cascaded devices
   int devices=lc.getDeviceCount();
   
+  /*
+    In order to get the addresses to index properly, you must invert the address call for
+    half of the matrices. Address 7 is parallel to 1 with inverted row/col coordinates, 2 is
+    parallel to 6, so on and so forth. (0,0) on addresses 0-3 is (7,7) on 4-7
+  */
   //we have to init all devices in a loop
+  int addInv = 7;
   for(int row=0;row<8;row++) {
     for(int col=0;col<8;col++) {
+      addInv = 7;
       for(int address=0;address<devices/2;address++) {
-        for(int addInv=7; addInv>devices/2; addInv--)
-        {
-          delay(delaytime);
+        
+         // delay(delaytime);
         lc.setLed(address,row,col,true);
-        lc.setLed(addInv,row,col,true);
+        lc.setLed(addInv,7-row,7-col,true);
         delay(delaytime);
         lc.setLed(address,row,col,false);
-        lc.setLed(addInv,row,col,false);
-        }
+        lc.setLed(addInv,7-row,7-col,false);
+        addInv--;
         
       }
     }
